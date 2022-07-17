e2b3f1cfb4f7365ad12545aaa21f0b77c8dde25f
Loris Reynaud <loris.reynaud@gmail.com>
2022-07-17 19:22:15 +0200

    navbar links

--- config/routes.rb
+++ config/routes.rb
@@ -5,11 +5,11 @@ Rails.application.routes.draw do
 
   get "art-fair", to: "pages#art_fair"
   get "art-fair-programm", to: "pages#art_fair_programm"
-  get "art-fair-exhibitors", to: "art_fair_exhibitors#index"
   get "design", to: "pages#design"
   get "design-programm", to: "pages#design_programm"
-  get "design-exhibitors", to: "design_exhibitors#index"
   get "registration", to: "pages#registration"
 
+  resources :art_fair_exhibitors, only: %i(index)
+  resources :design_exhibitors, only: %i(index)
   resources :partners, only: %i(index new edit update create destroy)
 end
