d936d858c472a699a262eadb4295a756cc7afd9d
Loris Reynaud <loris.reynaud@gmail.com>
2022-07-18 20:49:13 +0200

    commented all wrong migrations

--- db/migrate/20220709155111_remove_exhibitor_categories_table.rb
+++ db/migrate/20220709155111_remove_exhibitor_categories_table.rb
@@ -1,6 +1,6 @@
 class RemoveExhibitorCategoriesTable < ActiveRecord::Migration[7.0]
   def change
-    remove_column :exhibitor_categories, :category_references
-    add_reference :exhibitor_categories, :category, index: true
+    # remove_column :exhibitor_categories, :category_references
+    # add_reference :exhibitor_categories, :category, index: true
   end
 end
--- db/migrate/20220710094400_add_programm_to_categories.rb
+++ db/migrate/20220710094400_add_programm_to_categories.rb
@@ -1,5 +1,5 @@
 class AddProgrammToCategories < ActiveRecord::Migration[7.0]
   def change
-    add_column :categories, :programm, :text
+    # add_column :categories, :programm, :text
   end
 end
