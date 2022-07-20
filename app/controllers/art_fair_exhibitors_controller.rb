384fe6b8341064ed940735dc0842f3bfa29c8b7b
Loris Reynaud <loris.reynaud@gmail.com>
2022-07-20 19:14:33 +0200

    index exhibitors

--- app/controllers/art_fair_exhibitors_controller.rb
+++ app/controllers/art_fair_exhibitors_controller.rb
@@ -2,6 +2,6 @@ class ArtFairExhibitorsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @art_fair_exhibitors = ArtFairExhibitor.all
+    @art_fair_exhibitors = ArtFairExhibitor.all.order("id ASC")
   end
 end
