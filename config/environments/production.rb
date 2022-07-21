2eaacf729e41999a7c569e99a12ae931c700c77d
Loris Reynaud <loris.reynaud@gmail.com>
2022-07-21 22:54:54 +0200

    mailer things

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.default_url_options = { host: "https://bienvenue.art" }
+  config.action_mailer.default_url_options = { host: "https://bienvenue-staging.herokuapp.com/" }
   # Settings specified here will take precedence over those in config/application.rb.
 
   # Code is not reloaded between requests.
