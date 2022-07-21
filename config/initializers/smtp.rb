e0f6a833774fae55e6b84eb08a77e7b425542b89
Loris Reynaud <loris.reynaud@gmail.com>
2022-07-21 22:18:44 +0200

    smtp port changed

--- config/initializers/smtp.rb
+++ config/initializers/smtp.rb
@@ -1,6 +1,6 @@
 ActionMailer::Base.smtp_settings = {
   address: "mail.privateemail.com",
-  port: 587,
+  port: 465,
   domain: 'privateemail.com',
   user_name: ENV['EMAIL_ADDRESS'],
   password: ENV['EMAIL_PASSWORD'],
