87506966c7067dd69c5f2036a604702a21762f3e
Loris Reynaud <loris.reynaud@gmail.com>
2022-07-26 13:55:58 +0200

    hosts cleared in dev env for ngrok

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -1,6 +1,8 @@
 require "active_support/core_ext/integer/time"
 
 Rails.application.configure do
+  config.hosts.clear
+
   config.action_mailer.default_url_options = { host: "http://localhost:3000" }
   # Settings specified here will take precedence over those in config/application.rb.
 
@@ -68,6 +70,4 @@ Rails.application.configure do
 
   # Uncomment if you wish to allow Action Cable access from any origin.
   # config.action_cable.disable_request_forgery_protection = true
-
-  config.hosts << ".ngrok.io"
 end
