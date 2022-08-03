1fcb37728c7fe2677f3c6caa337a8d29d3b57cb5
Loris Reynaud <loris.reynaud@gmail.com>
2022-08-03 17:38:41 +0200

    i18n works without home page

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,13 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+
+  before_action :set_locale
+
+  def set_locale
+    I18n.locale = params[:locale] || I18n.default_locale
+  end
+
+  def default_url_options
+    { locale: I18n.locale }
+  end
 end
