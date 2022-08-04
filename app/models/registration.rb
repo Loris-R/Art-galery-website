53e1371eaabe72825a0e2fdf01a331d4f09a7802
Loris Reynaud <loris.reynaud@gmail.com>
2022-08-04 13:10:55 +0200

    i18n locales integrated on views

--- app/models/registration.rb
+++ app/models/registration.rb
@@ -1,7 +1,10 @@
 class Registration < ApplicationRecord
-  TITLES = ["Mr.", "Mrs."]
-  STATUS = ["Collector", "Press", "Curator", "Visitor"]
+  TITLES_EN = ["Mr.", "Mrs."]
+  STATUS_EN = ["Collector", "Press", "Curator", "Visitor"]
   CATEGORIES = ["Art", "Design"]
+
+  TITLES_FR = ["Mr.", "Mme."]
+  STATUS_FR = ["Collectionneur", "Press", "Curateur", "Visiteur"]
 
   validates :title, :first_name, :last_name, :status, :email, :category, presence: true
 end
