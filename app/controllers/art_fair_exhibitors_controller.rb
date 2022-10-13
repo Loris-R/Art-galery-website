bede8db3badbb57a6e1d52b61e1103c03e7fe75c
erika fonseca <fonsecarika@gmail.com>
2022-10-13 11:16:08 +0300

    alphabetic order

--- app/controllers/art_fair_exhibitors_controller.rb
+++ app/controllers/art_fair_exhibitors_controller.rb
@@ -2,6 +2,6 @@ class ArtFairExhibitorsController < ApplicationController
   skip_before_action :authenticate_user!
 
   def index
-    @art_fair_exhibitors = ArtFairExhibitor.all.order("id ASC")
+    @art_fair_exhibitors = ArtFairExhibitor.all.sort_by { |h| h[:artist_name] }
   end
 end
